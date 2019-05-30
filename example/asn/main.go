05de2a6cdcc1af743ee523e412e5d5826c489c7a
Petr Shevtsov <petr@ipinfo.io>
2019-05-30 12:52:49 +0300

    Initial commit

---
+++ example/asn/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ipinfo/go-ipinfo/ipinfo"
+)
+
+func main() {
+	// Get access token by signing up a free account at https://ipinfo.io/signup
+	// Provide token as an environment variable `TOKEN`,
+	// e.g. TOKEN="XXXXXXXXXXXXXX" go run main.go
+	authTransport := ipinfo.AuthTransport{
+		Token: os.Getenv("TOKEN"),
+	}
+	httpClient := authTransport.Client()
+	client := ipinfo.NewClient(httpClient)
+	asnInfo, err := client.ASN("AS7922")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(asnInfo)
+}
---
+++ ipinfo/asn.go
@@ -0,0 +1,56 @@
+package ipinfo
+
+import (
+	"strings"
+	"time"
+)
+
+// ASNInfo represents ASN details.
+type ASNInfo struct {
+	ASN         string      `json:"asn"`
+	Name        string      `json:"name"`
+	Country     string      `json:"country"`
+	Allocated   time.Time   `json:"allocated"`
+	Registry    string      `json:"registry"`
+	Domain      string      `json:"domain"`
+	NumberOfIPs uint64      `json:"num_ips"`
+	Type        string      `json:"type"`
+	Prefixes    []ASNPrefix `json:"prefixes"`
+	Prefixes6   []ASNPrefix `json:"prefixes6"`
+}
+
+// ASNPrefix represents an ASN prefix.
+type ASNPrefix struct {
+	Netblock string `json:"netblock"`
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Country  string `json:"country"`
+}
+
+// InvalidASNError is reported when the invalid ASN was specified.
+type InvalidASNError struct {
+	ASN string
+}
+
+func (err *InvalidASNError) Error() string {
+	return "invalid ASN: " + err.ASN
+}
+
+// ASN returns the details for the specified ASN.
+func ASN(asn string) (*ASNInfo, error) {
+	return c.ASN(asn)
+}
+
+// ASN returns the details for the specified ASN.
+func (c *Client) ASN(asn string) (*ASNInfo, error) {
+	if !strings.HasPrefix(asn, "AS") {
+		return nil, &InvalidASNError{ASN: asn}
+	}
+	req, err := c.NewRequest(asn + "/json")
+	if err != nil {
+		return nil, err
+	}
+	v := new(ASNInfo)
+	_, err = c.Do(req, v)
+	return v, err
+}
