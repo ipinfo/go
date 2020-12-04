e1683623375a6242e546c35381d9108717496f92
Uman Shahzad <uman@mslm.io>
2020-12-04 14:42:17 +0500

    document cache

--- ipinfo/cache.go
+++ ipinfo/cache.go
@@ -6,17 +6,23 @@ import (
 	"github.com/ipinfo/go/ipinfo/cache"
 )
 
+// Cache represents the internal cache used by the IPinfo client.
 type Cache struct {
 	cache.Interface
 	requestLocks sync.Map
 }
 
+// NewCache creates a new cache given a specific engine.
 func NewCache(engine cache.Interface) *Cache {
 	return &Cache{Interface: engine}
 }
 
+// EvaluatorFunc is a function called when a cache miss occurs to fill the
+// cache with the missing value for a key.
 type EvaluatorFunc func() (interface{}, error)
 
+// GetOrRequest attempts to first retrieve the value from the cache, and if it
+// isn't found, calls `evaluator` to get it and backfill the cache.
 func (c *Cache) GetOrRequest(
 	key string,
 	evaluator EvaluatorFunc,
