5fb0eb9e144b092bdc1c97373492a72973d66248
Uman Shahzad <uman@mslm.io>
2020-12-04 14:50:38 +0500

    comments for cache subdir

--- ipinfo/cache/interface.go
+++ ipinfo/cache/interface.go
@@ -3,9 +3,12 @@ package cache
 import "errors"
 
 var (
+	// ErrNotFound means that the key was not found.
 	ErrNotFound = errors.New("key not found")
 )
 
+// Interface is the cache interface that all cache implementations must adhere
+// to at the minimum to be usable in the IPinfo client.
 type Interface interface {
 	Get(key string) (interface{}, error)
 	Set(key string, value interface{}) error
