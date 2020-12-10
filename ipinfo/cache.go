b0cda0d4e50e5e8f23c87e9e607d2ad9037b62c7
Uman Shahzad <uman@mslm.io>
2020-12-10 16:14:41 +0500

    require cache impls to be concurrency-safe.
    note that the in-mem impl is already concurrency-safe
    because the underlying library does use locks appropriately.

--- ipinfo/cache.go
+++ ipinfo/cache.go
@@ -16,35 +16,3 @@ type Cache struct {
 func NewCache(engine cache.Interface) *Cache {
 	return &Cache{Interface: engine}
 }
-
-// EvaluatorFunc is a function called when a cache miss occurs to fill the
-// cache with the missing value for a key.
-type EvaluatorFunc func() (interface{}, error)
-
-// GetOrRequest attempts to first retrieve the value from the cache, and if it
-// isn't found, calls `evaluator` to get it and backfill the cache.
-func (c *Cache) GetOrRequest(
-	key string,
-	evaluator EvaluatorFunc,
-) (interface{}, error) {
-	value, _ := c.requestLocks.LoadOrStore(key, &sync.Mutex{})
-	mutex := value.(*sync.Mutex)
-	mutex.Lock()
-	defer func() {
-		c.requestLocks.Delete(key)
-		mutex.Unlock()
-	}()
-	value, err := c.Get(key)
-	if err == nil {
-		return value, nil
-	}
-	if err == cache.ErrNotFound {
-		value, err := evaluator()
-		if err != nil {
-			return nil, err
-		}
-		err = c.Set(key, value)
-		return value, err
-	}
-	return nil, err
-}
