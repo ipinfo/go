345eea4e8d9bfb23d6c2ac2d6ed62a3fe7134f69
Uman Shahzad <uman@mslm.io>
2020-12-10 16:15:40 +0500

    no longer needed; see prev commit

--- ipinfo/cache.go
+++ ipinfo/cache.go
@@ -1,15 +1,12 @@
 package ipinfo
 
 import (
-	"sync"
-
 	"github.com/ipinfo/go/ipinfo/cache"
 )
 
 // Cache represents the internal cache used by the IPinfo client.
 type Cache struct {
 	cache.Interface
-	requestLocks sync.Map
 }
 
 // NewCache creates a new cache given a specific engine.
